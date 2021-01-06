c2b428db7357334894a3a7411af3be978bd23e60
HoukasaurusRex <jt@houk.space>
2021-01-07 01:27:28 +0800

    fix: workbox plugin options

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -39,42 +39,44 @@ export default {
     {
       resolve: 'gatsby-plugin-offline',
       options: {
-        runtimeCaching: [
-          {
-            urlPattern: /\/$/,
-            handler: 'StaleWhileRevalidate'
-          },
-          {
-            urlPattern: /\.mp3$/,
-            handler: 'CacheFirst'
-          },
-          {
-            // Use cacheFirst since these don't need to be revalidated (same RegExp
-            // and same reason as above)
-            urlPattern: /(\.js$|\.css$|static\/)/,
-            handler: 'CacheFirst'
-          },
-          {
-            // page-data.json files, static query results and app-data.json
-            // are not content hashed
-            urlPattern: /^https?:.*\/page-data\/.*\.json/,
-            handler: 'StaleWhileRevalidate'
-          },
-          {
-            // Add runtime caching of various other page resources
-            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-            handler: 'StaleWhileRevalidate'
-          },
-          {
-            // Google Fonts CSS (doesn't end in .css so we need to specify it)
-            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-            handler: 'StaleWhileRevalidate'
-          },
-          {
-            urlPattern: /^https?:\/\/api\.houk\.space\/feed-to-json.*/,
-            handler: 'CacheFirst'
-          }
-        ]
+        workboxConfig: {
+          runtimeCaching: [
+            {
+              urlPattern: /\/$/,
+              handler: 'StaleWhileRevalidate'
+            },
+            {
+              urlPattern: /\.mp3$/,
+              handler: 'CacheFirst'
+            },
+            {
+              // Use cacheFirst since these don't need to be revalidated (same RegExp
+              // and same reason as above)
+              urlPattern: /(\.js$|\.css$|static\/)/,
+              handler: 'CacheFirst'
+            },
+            {
+              // page-data.json files, static query results and app-data.json
+              // are not content hashed
+              urlPattern: /^https?:.*\/page-data\/.*\.json/,
+              handler: 'StaleWhileRevalidate'
+            },
+            {
+              // Add runtime caching of various other page resources
+              urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+              handler: 'StaleWhileRevalidate'
+            },
+            {
+              // Google Fonts CSS (doesn't end in .css so we need to specify it)
+              urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+              handler: 'StaleWhileRevalidate'
+            },
+            {
+              urlPattern: /^https?:\/\/api\.houk\.space\/feed-to-json.*/,
+              handler: 'CacheFirst'
+            }
+          ]
+        }
       }
     }
   ]
