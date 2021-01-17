2afd35603f203c97b4fe2ce1fa9290169f88a445
HoukasaurusRex <jt@houk.space>
2021-01-17 14:59:46 +0800

    chore: newest first

--- src/components/episodes.tsx
+++ src/components/episodes.tsx
@@ -6,7 +6,7 @@ import { feedData as fd } from '../../types/media-types'
 import './loading-ellipses.css'
 
 export default function Episodes({ feedData }: { feedData: fd }): JSX.Element {
-  const episodes = feedData.items?.reverse().map((item, i) => {
+  const episodes = feedData.items?.map((item, i) => {
     const selfHostedFile = i === 0 ? `${item.guid}.mp3` : ''
     return <AudioCard key={item.guid} item={item} selfHostedFile={selfHostedFile} />
   }) || <Text className="loading">loading episodes</Text>
