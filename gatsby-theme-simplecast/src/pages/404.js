e79a4c7c22521b0942093527b050ce666b5a892a
HoukasaurusRex <jt@houk.space>
2021-08-15 19:57:55 +0800

    chore: added layout changes to 404

--- gatsby-theme-simplecast/src/pages/404.js
+++ gatsby-theme-simplecast/src/pages/404.js
@@ -1,14 +1,13 @@
-import React from "react"
-
-import Layout from "../components/layout"
+/** @jsx jsx */
+import { jsx, Flex } from "theme-ui"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Flex sx={{ px: 30, height: '100vh', flexDirection: 'column', justifyContent: 'center' }}>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </Flex>
 )
 
 export default NotFoundPage
