9432a4f4729f5221f8e5d1f1fff214775d64fbc8
JT Houk <jt.houk@lightspeedhq.com>
2026-03-11 13:02:59 -0400

    chore: remove unused useCallback import

--- src/components/PlayerH5.tsx
+++ src/components/PlayerH5.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { useStore } from '@nanostores/react'
 import H5AudioPlayer from 'react-h5-audio-player'
